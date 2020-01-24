a005cf6f1b45090dd078c2ba499ff0c6d40d070d
julnolle <julnolle@student.42.fr>
2020-01-24 11:47:49 +0100

    add makefile hamdle

--- compile_project.sh
+++ compile_project.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Set "yes" if you are using makefile, "no" if not
+makefile="no"
+
+# Choose the commande to execute your binary, with the aruguments if needed
+exec="./a.out lorem"
+
+
+# ============================================================
+# In case not using Makefile ("makefile="no"")
+# ============================================================
+
 # Choose compilator
 compilator="gcc"
 
@@ -9,13 +20,22 @@ files_to_test="get_next_line.c get_next_line_utils.c main.c"
 # Lhoose the flages for compilation
 flags="-Wall -Wextra -Werror -D BUFFER_SIZE=32"
 
-# Choose the commande to execute your binary, with the aruguments if needed
-exec="./a.out lorem"
+# ============================================================
 
 
-# Compilation and valgrind test
+
+# Go into test folder in container
 cd /test
+
+# Run compilation
+if [ $makefile = "yes" ]
+then
+make 
+else
 $compilator -g $flags $files_to_test 
+fi
+
+# Run valgrind test
 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes $exec
 
 # Uncomment if you need to launch bash inside docker:
